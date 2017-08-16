e6265ce9f1b62f81276c7c19238a2f1e2cf21454
Daobataotie <caoruiwensao@qq.com>
2017-08-17 00:47:13 +0800

    u

--- Solution1.root/Book.UI/Hr/Salary/Salaryset/AnnualSalaryRO.designer.cs
+++ Solution1.root/Book.UI/Hr/Salary/Salaryset/AnnualSalaryRO.designer.cs
@@ -29,17 +29,17 @@ namespace Book.UI.Hr.Salary.Salaryset
         private void InitializeComponent()
         {
             this.Detail = new DevExpress.XtraReports.UI.DetailBand();
-            this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
-            this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
-            this.ReportHeader = new DevExpress.XtraReports.UI.ReportHeaderBand();
-            this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
-            this.xrLabel2 = new DevExpress.XtraReports.UI.XRLabel();
-            this.lblAnnual = new DevExpress.XtraReports.UI.XRLabel();
-            this.lblName = new DevExpress.XtraReports.UI.XRLabel();
             this.xrTable1 = new DevExpress.XtraReports.UI.XRTable();
             this.xrTableRow1 = new DevExpress.XtraReports.UI.XRTableRow();
             this.xrTableCell1 = new DevExpress.XtraReports.UI.XRTableCell();
             this.xrLabel4 = new DevExpress.XtraReports.UI.XRLabel();
+            this.lblName = new DevExpress.XtraReports.UI.XRLabel();
+            this.TopMargin = new DevExpress.XtraReports.UI.TopMarginBand();
+            this.BottomMargin = new DevExpress.XtraReports.UI.BottomMarginBand();
+            this.ReportHeader = new DevExpress.XtraReports.UI.ReportHeaderBand();
+            this.lblAnnual = new DevExpress.XtraReports.UI.XRLabel();
+            this.xrLabel2 = new DevExpress.XtraReports.UI.XRLabel();
+            this.xrLabel1 = new DevExpress.XtraReports.UI.XRLabel();
             ((System.ComponentModel.ISupportInitialize)(this.xrTable1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
             // 
@@ -48,94 +48,19 @@ namespace Book.UI.Hr.Salary.Salaryset
             this.Detail.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
             this.xrTable1});
             this.Detail.Dpi = 254F;
-            this.Detail.HeightF = 351.8958F;
+            this.Detail.HeightF = 608.5417F;
             this.Detail.Name = "Detail";
             this.Detail.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 254F);
             this.Detail.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
             // 
-            // TopMargin
-            // 
-            this.TopMargin.Dpi = 254F;
-            this.TopMargin.HeightF = 80F;
-            this.TopMargin.Name = "TopMargin";
-            this.TopMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 254F);
-            this.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
-            // 
-            // BottomMargin
-            // 
-            this.BottomMargin.Dpi = 254F;
-            this.BottomMargin.HeightF = 80F;
-            this.BottomMargin.Name = "BottomMargin";
-            this.BottomMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 254F);
-            this.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
-            // 
-            // ReportHeader
-            // 
-            this.ReportHeader.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
-            this.lblAnnual,
-            this.xrLabel2,
-            this.xrLabel1});
-            this.ReportHeader.Dpi = 254F;
-            this.ReportHeader.HeightF = 137.795F;
-            this.ReportHeader.Name = "ReportHeader";
-            // 
-            // xrLabel1
-            // 
-            this.xrLabel1.Dpi = 254F;
-            this.xrLabel1.Font = new System.Drawing.Font("新細明體", 12F);
-            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(10.58333F, 0F);
-            this.xrLabel1.Name = "xrLabel1";
-            this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 5, 0, 0, 254F);
-            this.xrLabel1.SizeF = new System.Drawing.SizeF(1976.438F, 71.4375F);
-            this.xrLabel1.StylePriority.UseFont = false;
-            this.xrLabel1.StylePriority.UseTextAlignment = false;
-            this.xrLabel1.Text = "薪    工    資    伙    食    代    金    表";
-            this.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
-            // 
-            // xrLabel2
-            // 
-            this.xrLabel2.Dpi = 254F;
-            this.xrLabel2.LocationFloat = new DevExpress.Utils.PointFloat(1558.396F, 71.4375F);
-            this.xrLabel2.Name = "xrLabel2";
-            this.xrLabel2.Padding = new DevExpress.XtraPrinting.PaddingInfo(2, 2, 0, 0, 96F);
-            this.xrLabel2.SizeF = new System.Drawing.SizeF(174.625F, 58.42F);
-            this.xrLabel2.StylePriority.UseTextAlignment = false;
-            this.xrLabel2.Text = "年度：";
-            this.xrLabel2.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
-            // 
-            // lblAnnual
-            // 
-            this.lblAnnual.Borders = DevExpress.XtraPrinting.BorderSide.Bottom;
-            this.lblAnnual.Dpi = 254F;
-            this.lblAnnual.LocationFloat = new DevExpress.Utils.PointFloat(1733.021F, 71.4375F);
-            this.lblAnnual.Name = "lblAnnual";
-            this.lblAnnual.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 5, 0, 0, 254F);
-            this.lblAnnual.SizeF = new System.Drawing.SizeF(254F, 58.42F);
-            this.lblAnnual.StylePriority.UseBorders = false;
-            this.lblAnnual.StylePriority.UseTextAlignment = false;
-            this.lblAnnual.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
-            // 
-            // lblName
-            // 
-            this.lblName.Borders = DevExpress.XtraPrinting.BorderSide.None;
-            this.lblName.Dpi = 254F;
-            this.lblName.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
-            this.lblName.Name = "lblName";
-            this.lblName.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 5, 0, 0, 254F);
-            this.lblName.SizeF = new System.Drawing.SizeF(254F, 58.42F);
-            this.lblName.StylePriority.UseBorders = false;
-            this.lblName.StylePriority.UseTextAlignment = false;
-            this.lblName.Text = "lblName";
-            this.lblName.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
-            // 
             // xrTable1
             // 
             this.xrTable1.Dpi = 254F;
-            this.xrTable1.LocationFloat = new DevExpress.Utils.PointFloat(10.58333F, 0F);
+            this.xrTable1.LocationFloat = new DevExpress.Utils.PointFloat(1F, 0F);
             this.xrTable1.Name = "xrTable1";
             this.xrTable1.Rows.AddRange(new DevExpress.XtraReports.UI.XRTableRow[] {
             this.xrTableRow1});
-            this.xrTable1.SizeF = new System.Drawing.SizeF(1976.438F, 351.8958F);
+            this.xrTable1.SizeF = new System.Drawing.SizeF(2862.792F, 608.5417F);
             // 
             // xrTableRow1
             // 
@@ -168,6 +93,81 @@ namespace Book.UI.Hr.Salary.Salaryset
             this.xrLabel4.SizeF = new System.Drawing.SizeF(254F, 129.8575F);
             this.xrLabel4.Text = "xrLabel4";
             // 
+            // lblName
+            // 
+            this.lblName.Borders = DevExpress.XtraPrinting.BorderSide.None;
+            this.lblName.Dpi = 254F;
+            this.lblName.LocationFloat = new DevExpress.Utils.PointFloat(0F, 0F);
+            this.lblName.Name = "lblName";
+            this.lblName.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 5, 0, 0, 254F);
+            this.lblName.SizeF = new System.Drawing.SizeF(254F, 58.42F);
+            this.lblName.StylePriority.UseBorders = false;
+            this.lblName.StylePriority.UseTextAlignment = false;
+            this.lblName.Text = "lblName";
+            this.lblName.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
+            // 
+            // TopMargin
+            // 
+            this.TopMargin.Dpi = 254F;
+            this.TopMargin.HeightF = 80F;
+            this.TopMargin.Name = "TopMargin";
+            this.TopMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 254F);
+            this.TopMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // BottomMargin
+            // 
+            this.BottomMargin.Dpi = 254F;
+            this.BottomMargin.HeightF = 80F;
+            this.BottomMargin.Name = "BottomMargin";
+            this.BottomMargin.Padding = new DevExpress.XtraPrinting.PaddingInfo(0, 0, 0, 0, 254F);
+            this.BottomMargin.TextAlignment = DevExpress.XtraPrinting.TextAlignment.TopLeft;
+            // 
+            // ReportHeader
+            // 
+            this.ReportHeader.Controls.AddRange(new DevExpress.XtraReports.UI.XRControl[] {
+            this.lblAnnual,
+            this.xrLabel2,
+            this.xrLabel1});
+            this.ReportHeader.Dpi = 254F;
+            this.ReportHeader.HeightF = 137.795F;
+            this.ReportHeader.Name = "ReportHeader";
+            // 
+            // lblAnnual
+            // 
+            this.lblAnnual.Borders = DevExpress.XtraPrinting.BorderSide.Bottom;
+            this.lblAnnual.Dpi = 254F;
+            this.lblAnnual.LocationFloat = new DevExpress.Utils.PointFloat(2608.792F, 71.4375F);
+            this.lblAnnual.Name = "lblAnnual";
+            this.lblAnnual.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 5, 0, 0, 254F);
+            this.lblAnnual.SizeF = new System.Drawing.SizeF(254.9998F, 58.42F);
+            this.lblAnnual.StylePriority.UseBorders = false;
+            this.lblAnnual.StylePriority.UseTextAlignment = false;
+            this.lblAnnual.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
+            // 
+            // xrLabel2
+            // 
+            this.xrLabel2.Dpi = 254F;
+            this.xrLabel2.LocationFloat = new DevExpress.Utils.PointFloat(2434.167F, 71.4375F);
+            this.xrLabel2.Name = "xrLabel2";
+            this.xrLabel2.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 5, 0, 0, 254F);
+            this.xrLabel2.SizeF = new System.Drawing.SizeF(174.625F, 58.42F);
+            this.xrLabel2.StylePriority.UseTextAlignment = false;
+            this.xrLabel2.Text = "年度：";
+            this.xrLabel2.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleLeft;
+            // 
+            // xrLabel1
+            // 
+            this.xrLabel1.Dpi = 254F;
+            this.xrLabel1.Font = new System.Drawing.Font("新細明體", 12F);
+            this.xrLabel1.LocationFloat = new DevExpress.Utils.PointFloat(10.58333F, 0F);
+            this.xrLabel1.Name = "xrLabel1";
+            this.xrLabel1.Padding = new DevExpress.XtraPrinting.PaddingInfo(5, 5, 0, 0, 254F);
+            this.xrLabel1.SizeF = new System.Drawing.SizeF(2853.209F, 71.4375F);
+            this.xrLabel1.StylePriority.UseFont = false;
+            this.xrLabel1.StylePriority.UseTextAlignment = false;
+            this.xrLabel1.Text = "薪    工    資    伙    食    代    金    表";
+            this.xrLabel1.TextAlignment = DevExpress.XtraPrinting.TextAlignment.MiddleCenter;
+            // 
             // AnnualSalaryRO
             // 
             this.Bands.AddRange(new DevExpress.XtraReports.UI.Band[] {
@@ -177,9 +177,10 @@ namespace Book.UI.Hr.Salary.Salaryset
             this.ReportHeader});
             this.Dpi = 254F;
             this.Font = new System.Drawing.Font("新細明體", 9.75F);
-            this.Margins = new System.Drawing.Printing.Margins(80, 80, 80, 80);
-            this.PageHeight = 2794;
-            this.PageWidth = 2159;
+            this.Margins = new System.Drawing.Printing.Margins(50, 50, 80, 80);
+            this.PageHeight = 2100;
+            this.PageWidth = 2970;
+            this.PaperKind = System.Drawing.Printing.PaperKind.Custom;
             this.ReportUnit = DevExpress.XtraReports.UI.ReportUnit.TenthsOfAMillimeter;
             this.SnapGridSize = 31.75F;
             this.Version = "10.2";
