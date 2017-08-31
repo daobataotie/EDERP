e1397a494112d92176db58804a0c56b8326f6be2
Daobataotie <caoruiwensao@qq.com>
2017-08-31 23:45:20 +0800

    u

--- Solution1.root/Book.UI/produceManager/PCExportReportANSI/ProductTestRO.cs
+++ Solution1.root/Book.UI/produceManager/PCExportReportANSI/ProductTestRO.cs
@@ -20,7 +20,7 @@ namespace Book.UI.produceManager.PCExportReportANSI
             this.TCCustomerShortName.Text = pcDataInput.CustomerShortName;
             this.TCProduct.Text = pcDataInput.Product == null ? "" : pcDataInput.Product.ToString();
             this.TCInvoiceCusId.Text = pcDataInput.InvoiceCusId;
-            this.TCTotalQuantity.Text = pcDataInput.OrderQuantity.HasValue ? pcDataInput.OrderQuantity.ToString() : "";
+            this.TCTotalQuantity.Text = pcDataInput.OrderQuantity.HasValue ? pcDataInput.OrderQuantity.Value.ToString("f0") : "";
             //this.TCTotalQuantity.Text = pcDataInput.PronoteHeader.DetailsSum.HasValue ? pcDataInput.PronoteHeader.DetailsSum.ToString() : "";
             this.TCTestQuantity.Text = pcDataInput.TestQuantity.HasValue ? pcDataInput.TestQuantity.ToString() : "";
             this.TCDate.Text = pcDataInput.PCDataInputDate.HasValue ? pcDataInput.PCDataInputDate.Value.ToString("yyyy-MM-dd") : "";
