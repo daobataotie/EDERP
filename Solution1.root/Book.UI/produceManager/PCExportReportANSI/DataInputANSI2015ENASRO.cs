77d767d681ea5dfd436be4887368da3385b6407c
daobataotie <caoruiwensao@qq.com>
2017-09-01 22:22:37 +0800

    u

---
+++ Solution1.root/Book.UI/produceManager/PCExportReportANSI/DataInputANSI2015ENASRO.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Book.UI.produceManager.PCExportReportANSI
+{
+    public partial class DataInputANSI2015ENASRO : DevExpress.XtraReports.UI.XtraReport
+    {
+        public DataInputANSI2015ENASRO()
+        {
+            InitializeComponent();
+        }
+
+        public DataInputANSI2015ENASRO(Model.PCDataInput pcDataInput, Model.PCExportReportANSI pcExportReportANSI, Model.PCExportReportANSI pcEN, Model.PCExportReportANSI pcAS, int tag)
+            : this()
+        {
+            if (pcDataInput == null)
+                return;
+          
+            this.TCTSData.Text = pcDataInput.PCDataInputDate.HasValue ? pcDataInput.PCDataInputDate.Value.ToString("yyyy-MM-dd") : "";
+            this.TCTSQuantity.Text = pcDataInput.PCPerspectiveList.Count.ToString();
+            this.TCTSEmployee.Text = pcDataInput.Employee3 == null ? "" : pcDataInput.Employee3.ToString();
+
+            if (pcExportReportANSI != null)
+                this.xrSubreportANSI.ReportSource = new ANSI2015RO(pcExportReportANSI, tag);
+            if (pcEN != null)
+                this.xrSubreportEN.ReportSource = new CEENRO(pcEN, tag);
+            if (pcAS != null)
+                this.xrSubreportAS.ReportSource = new ASRO2017(pcAS, tag);
+            this.xrSubreportProductTest.ReportSource = new ProductTestRO(pcDataInput);
+            this.xrSubreportProductTest.ReportSource = new ProductTestRO(pcDataInput);
+            this.xrSubreportPCOpticalMachine.ReportSource = new PCOpticalMachineRO(pcDataInput.PCOpticalMachineList, pcDataInput);
+            this.xrSubreportPCHaze.ReportSource = new PCHazeRO(pcDataInput.PCHazeList, pcDataInput);
+        }
+
+        private void DataInputANSIRO_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            //RO ro = this.xrSubreportANSI.ReportSource as RO;
+            //ro.ANSI = pcExportReportANSI;
+        }
+
+    }
+}
---
+++ Solution1.root/Book.UI/produceManager/PCExportReportANSI/DataInputANSI2015ENRO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace Book.UI.produceManager.PCExportReportANSI
+{
+    public partial class DataInputANSI2015ENRO : DevExpress.XtraReports.UI.XtraReport
+    {
+        public DataInputANSI2015ENRO()
+        {
+            InitializeComponent();
+        }
+
+        public DataInputANSI2015ENRO(Model.PCDataInput pcDataInput, Model.PCExportReportANSI pcExportReportANSI,Model.PCExportReportANSI pcEN,int tag)
+            : this()
+        {
+            if (pcDataInput == null)
+                return;
+
+            this.TCTSData.Text = pcDataInput.PCDataInputDate.HasValue ? pcDataInput.PCDataInputDate.Value.ToString("yyyy-MM-dd") : "";
+            this.TCTSQuantity.Text = pcDataInput.PCPerspectiveList.Count.ToString();
+            this.TCTSEmployee.Text = pcDataInput.Employee3 == null ? "" : pcDataInput.Employee3.ToString();
+
+            if (pcExportReportANSI != null)
+                this.xrSubreportANSI.ReportSource = new ANSI2015RO(pcExportReportANSI, tag);
+            if (pcEN != null)
+                this.xrSubreportEN.ReportSource = new CEENRO(pcEN, tag);
+            this.xrSubreportProductTest.ReportSource = new ProductTestRO(pcDataInput);
+            this.xrSubreportPCOpticalMachine.ReportSource = new PCOpticalMachineRO(pcDataInput.PCOpticalMachineList, pcDataInput);
+            this.xrSubreportPCHaze.ReportSource = new PCHazeRO(pcDataInput.PCHazeList, pcDataInput);
+        }
+
+        private void DataInputANSIRO_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e)
+        {
+            //RO ro = this.xrSubreportANSI.ReportSource as RO;
+            //ro.ANSI = pcExportReportANSI;
+        }
+
+    }
+}
