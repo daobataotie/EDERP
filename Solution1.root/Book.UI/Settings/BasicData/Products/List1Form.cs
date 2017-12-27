08682e9d72c4fbe5ed9ba44d42c0188eac39524e
Daobataotie <caoruiwensao@qq.com>
2017-12-27 20:54:20 +0800

    u

--- Solution1.root/Book.UI/Settings/BasicData/Products/List1Form.cs
+++ Solution1.root/Book.UI/Settings/BasicData/Products/List1Form.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
+using System.Linq;
 
 namespace Book.UI.Settings.BasicData.Products
 {
@@ -23,7 +24,10 @@ namespace Book.UI.Settings.BasicData.Products
     public partial class List1Form : BaseListForm
     {
         DataTable dt = new DataTable();
-        public  List1Form()
+        BL.CustomerProductPriceManager cpp = new Book.BL.CustomerProductPriceManager();
+        BL.SupplierProductManager spp = new Book.BL.SupplierProductManager();
+
+        public List1Form()
         {
 
             InitializeComponent();
@@ -36,7 +40,7 @@ namespace Book.UI.Settings.BasicData.Products
             string id = dt.Rows[this.bindingSource1.IndexOf(this.bindingSource1.Current)][0].ToString();
             EditForm._product = ((BL.ProductManager)this.manager).Get(id);
             //this.DialogResult = DialogResult.OK;
-            EditForm f = new EditForm(EditForm._product,"view");
+            EditForm f = new EditForm(EditForm._product, "view");
             f.ShowDialog(this);
         }
 
@@ -48,8 +52,78 @@ namespace Book.UI.Settings.BasicData.Products
         protected override void RefreshData()
         {
             //为了查询速度，暂将ProductSpecification，ProductDescription去掉
-            this.bindingSource1.DataSource = this.dt = ((BL.ProductManager)this.manager).Query("SELECT product.ProductId,product.Id,ProductName,ProductCategoryName,c.CustomerFullName ,CustomerProductName,isnull(StocksQuantity,0) StocksQuantity,ProductVersion FROM Product left join ProductCategory ca  on ca.ProductCategoryId=Product.ProductCategoryId left join Customer c on c.CustomerId=Product.CustomerId order by  ProductName", 300, "pro").Tables[0];
+            this.bindingSource1.DataSource = this.dt = ((BL.ProductManager)this.manager).Query("SELECT product.ProductId,product.Id,ProductName,ProductCategoryName,c.CustomerFullName ,CustomerProductName,isnull(StocksQuantity,0) StocksQuantity,ProductVersion,'' as Price FROM Product left join ProductCategory ca  on ca.ProductCategoryId=Product.ProductCategoryId left join Customer c on c.CustomerId=Product.CustomerId order by  ProductName", 300, "pro").Tables[0];
 
+            IList<Model.CustomerProductPrice> cppList = cpp.SelectAll();
+            IList<Model.SupplierProduct> sppList = spp.SelectAll();
+
+            #region 方案一
+            foreach (DataRow item in this.dt.Rows)
+            {
+                string productId = item["ProductId"].ToString();
+                string PriceRange = string.Empty;
+
+                Model.CustomerProductPrice cPrice = cppList.FirstOrDefault(C => C.ProductId == productId);
+                if (cPrice != null)
+                {
+                    PriceRange = cPrice.CustomerProductPriceRage;
+                    cppList.Remove(cPrice);
+                }
+                else
+                {
+                    Model.SupplierProduct sPrice = sppList.FirstOrDefault(S => S.ProductId == productId);
+                    if (sPrice != null)
+                    {
+                        PriceRange = sPrice.SupplierProductPriceRange;
+                        sppList.Remove(sPrice);
+                    }
+                }
+                string[] PriAndRange = string.IsNullOrEmpty(PriceRange) ? null : PriceRange.Split(',');
+
+                if (PriAndRange != null)
+                {
+                    item["Price"] = (string.IsNullOrEmpty(PriAndRange[0].Split('/')[2]) ? null : PriAndRange[0].Split('/')[2]);
+                }
+            }
+            #endregion
+
+            #region 方案二 更慢
+            //EnumerableRowCollection<DataRow> dtListAll = dt.AsEnumerable();
+            //EnumerableRowCollection<DataRow> dtListCustomer = dtListAll.Where(D => cppList.Any(C => C.ProductId == D.Field<string>("ProductId")));
+            //EnumerableRowCollection<DataRow> dtListSuppiler = dtListAll.Where(D => sppList.Any(C => C.ProductId == D.Field<string>("ProductId")));
+            //foreach (DataRow item in dtListCustomer)
+            //{
+            //    string productId = item["ProductId"].ToString();
+            //    string PriceRange = string.Empty;
+
+            //    Model.CustomerProductPrice cPrice = cppList.First(C => C.ProductId == productId);
+            //    PriceRange = cPrice.CustomerProductPriceRage;
+            //    cppList.Remove(cPrice);
+
+            //    string[] PriAndRange = string.IsNullOrEmpty(PriceRange) ? null : PriceRange.Split(',');
+
+            //    if (PriAndRange != null)
+            //    {
+            //        item["Price"] = (string.IsNullOrEmpty(PriAndRange[0].Split('/')[2]) ? null : PriAndRange[0].Split('/')[2]);
+            //    }
+            //}
+            //foreach (DataRow item in dtListSuppiler)
+            //{
+            //    string productId = item["ProductId"].ToString();
+            //    string PriceRange = string.Empty;
+
+            //    Model.SupplierProduct sPrice = sppList.First(S => S.ProductId == productId);
+            //    PriceRange = sPrice.SupplierProductPriceRange;
+            //    sppList.Remove(sPrice);
+
+            //    string[] PriAndRange = string.IsNullOrEmpty(PriceRange) ? null : PriceRange.Split(',');
+
+            //    if (PriAndRange != null)
+            //    {
+            //        item["Price"] = (string.IsNullOrEmpty(PriAndRange[0].Split('/')[2]) ? null : PriAndRange[0].Split('/')[2]);
+            //    }
+            //} 
+            #endregion
         }
 
         private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
